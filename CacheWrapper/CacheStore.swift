731682963ff873dd9f4097f696a5a3cd5d0132a6
ksatia <ksatia@gmail.com>
2016-09-19 14:05:11 -0400

    Change NSDate extension to support computed property via getter instead of using a stored property

--- CacheWrapper/CacheStore.swift
+++ CacheWrapper/CacheStore.swift
@@ -14,8 +14,24 @@ class CacheStore: NSObject {
     // we will have multiple caches for different object types (a cache for GSMcoordinates, a cache for UIImages for a particular part of an application -> the easiest way to differentiate between caches holding similar objects would be by name. MUST HAVE A NAME PROPERTY
     // We need support for clearing out an entire cache by name
     // We need support for clearing out all caches
-    // Need a file manager property, an NSURL property to route to that file directory
     // The cache that will be implemented is just an instance of NSCache (performance with hash functions) - should be internal constant since we don't want anybody importing the framework to have direct access to the internal structure
     //  Create a dispatch queue OR an NSOperation depending on what we end up doing with API response caching.
     // Consider using enum in CacheStore to determine how far off object expiration is. NSDate has NSDate.distanceFuture(), etc.
+    //start by initializing a cache using the internal cache variable backed by NSCache (instance of that cache). A file manager object is just used as a wrapper around lower level I/O operations and directory access. 
+    
+    let cache = NSCache<AnyObject, AnyObject>()
+    let fileManager = FileManager ()
+    let name: String
+    let directory: NSURL
+    
+    
+    // Declare as public so that the function is accessible to source files outside of this module (ie files being built by other developers who are importing this framework)
+    
+    public init(cacheName: String, directory: NSURL?, fileProtection: String? = nil) throws {
+        // attribute name to both class property for cache name
+        // NSCache has a name property already which seems to be used to differentiate between caches when a delegate method is called when objects are about to be evicted from the cache. This allows you to see which caches are evicting objects.
+        // We're using the name constant for creating a file path for each cache created.
+        self.name = cacheName
+        self.directory = directory!
+    }
 }
--- CacheWrapper/CachedObjectWrapper.swift
+++ CacheWrapper/CachedObjectWrapper.swift
@@ -10,23 +10,23 @@ import UIKit
 import Foundation
 
 //  UserDefaults && other methods of object graph persistence require being archived and encoded NSKeyedArchiver && the NSCoder protocol only work with subclasses of NSObject. As a result, we must subclass.
-class CachedObjectWrapper: NSObject, NSCoder {
-    let value: anyObject
+class CachedObjectWrapper: NSObject {
+    let value: AnyObject
     let expiration: NSDate
     let hasExpired: Bool = {
         return expiration.isInThePast
     }
     
     //designated initializer
-    init(value:anyObject, expiration: NSDate) {
+    init(value:AnyObject, expiration: NSDate) {
         self.value = value
         self.expiration = expiration
         super.init()
     }
 
     required init(coder aDecoder: NSCoder) {
-        guard let val = aDecoder.decodeObjectForKey("value"),
-            let expiry = aDecoder.decodeObjectForKey("expiration") as? NSDate else {
+        guard let val = aDecoder.decodeObject(forKey: "value"),
+            let expiry = aDecoder.decodeObject(forKey: "expiration") as? NSDate else {
                 return nil
         }
         self.value = val
@@ -35,19 +35,21 @@ class CachedObjectWrapper: NSObject, NSCoder {
     }
     
     func encodeWithEncoder(aCoder: NSCoder) {
-        aCoder.encodeObject(value, forKey: "value")
-        aCoder.encodeObject(expiration, forKey: "expiration")
+        aCoder.encode(value, forKey: "value")
+        aCoder.encode(expiration, forKey: "expiration")
     }
 }
     
     
-    extension NSDate {
-        var isInThePast: Bool = {
+extension NSDate {
+    var isInThePast: Bool {
+        get {
             return self.timeIntervalSinceNow < 0
         }
     }
-    
-    
+}
+
+
     //required nscoder init and encodeWithEncoder function. for the required nscover, assert that a constant can be assigned to the decoded.objectforKey("value"), assert that constant can be assigned to "expiration" else return. if you can proceed, assign constants to file-scoped vars and then call super init. THIS IS FOR DECODING.
     //to encode, call encodeWithEncoder and just call encodeObject on your coder being passed.
     
