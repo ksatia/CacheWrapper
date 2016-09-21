61caef68beb951bcdc5c805660b8b72cac06e66a
ksatia <ksatia@gmail.com>
2016-09-21 17:31:49 -0400

    Write convenience init using throws and account for error handling instead of using failable initializer

--- CacheWrapper/CacheStore.swift
+++ CacheWrapper/CacheStore.swift
@@ -19,15 +19,23 @@ class CacheStore: NSObject {
     // Consider using enum in CacheStore to determine how far off object expiration is. NSDate has NSDate.distanceFuture(), etc.
     //start by initializing a cache using the internal cache variable backed by NSCache (instance of that cache). A file manager object is just used as a wrapper around lower level I/O operations and directory access. 
     
+    //consider creating enum for expiration conditions. We can also create an enum for error handling and document for use in try/catch statements.
+    
+    
+    
+    
     let cache = NSCache<AnyObject, AnyObject>()
     let fileManager = FileManager ()
     let name: String
     var directory: URL
     
+    enum directoryErrors : Error {
+        case couldNotCreate
+    }
+    
     
     // Declare as public so that the function is accessible to source files outside of this module (ie files being built by other developers who are importing this framework)
-    
-    public init(cacheName: String, cacheDirectory: URL?, fileProtection: String? = nil) throws {
+    public init(cacheName: String, cacheDirectory: URL?, fileProtection: String? = nil) throws{
         
         // NSCache has a name property already which seems to be used to differentiate between caches when a delegate method is called when objects are about to be evicted from the cache. This allows you to see which caches are evicting objects. Not being used here.
         self.name = cacheName
@@ -41,9 +49,36 @@ class CacheStore: NSObject {
             let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
             self.directory = url.appendingPathComponent("ksatia.cache/\(cacheName)")
         }
+
+        // catch error in attempting to create directory at path -> CONSIDER WRITING TO FILE V DIRECTORY
+        do {
+            try fileManager.createDirectory(at: self.directory, withIntermediateDirectories: true, attributes: nil)
+        }
+        catch let error as NSError {
+            print(error.localizedDescription)
+            throw directoryErrors.couldNotCreate
+        }
         
-        // use fileManager property to create directory (try and catch the error, log a statement if unable to create directory)
-        
+        //set file protection values here -> default to max protection, include option in initializer for user specified protection level
     }
     
+    public convenience init(cacheName: String) throws {
+        do {
+            try self.init(cacheName:cacheName, cacheDirectory: nil, fileProtection: nil)
+        }
+        catch let error as NSError {
+            fatalError("Directory creation failed: \(error.localizedDescription)")
+        }
+    }
+ 
+    
+    
+}
+
+extension CacheStore {
+    var getCacheDirectory: URL {
+        get {
+            return self.directory
+        }
+    }
 }
